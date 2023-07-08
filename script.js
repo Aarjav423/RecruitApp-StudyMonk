1a5439c844d1e80364f363e66755f24c48324085
Aarjav Jain <jaarjav423@gmail.com>
2023-07-09 00:40:35 +0530

    RecruitApp-StudyMonk
    To design and develop a digital recruitment solution app aimed
    at providing a seamless hiring experience for multiple MNCs.

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+document.getElementById('searchForm').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevent form submission
+    var location = document.getElementById('location').value;
+    var jobRole = document.getElementById('jobRole').value;
+    // Perform search based on location and job role
+    // Retrieve and display search results in the 'searchResults' section
+  });
+  
